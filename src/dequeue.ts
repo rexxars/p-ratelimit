2026b26a04513f65825c338cf5db55bb10165d1c
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-12-31 13:37:46 +0100

    Remove unused methods from dequeue

--- src/dequeue.ts
+++ src/dequeue.ts
@@ -13,16 +13,11 @@ export class Dequeue<T> {
     return this._length;
   }
 
-  clear() {
-    this.head = this.tail = undefined;
-    this._length = 0;
-  }
-
   push(value: T) {
     const newNode: Node<T> = {
       value,
       prev: this.tail,
-      next: undefined
+      next: undefined,
     };
 
     if (this._length) {
@@ -31,36 +26,6 @@ export class Dequeue<T> {
     } else {
       this.head = this.tail = newNode;
     }
-    this._length++;
-  }
-
-  pop(): T {
-    if (!this._length) {
-      return undefined;
-    }
-    const result = this.tail;
-    this.tail = this.tail.prev;
-    this._length--;
-    if (!this._length) {
-      this.head = this.tail = undefined;
-    }
-    return result.value;
-  }
-
-  unshift(value: T) {
-    const newNode: Node<T> = {
-      value,
-      prev: undefined,
-      next: this.head
-    };
-
-    if (this._length) {
-      this.head.prev = newNode;
-      this.head = newNode;
-    } else {
-      this.head = this.tail = newNode;
-    }
-
     this._length++;
   }
 
@@ -80,13 +45,6 @@ export class Dequeue<T> {
   peekFront(): T {
     if (this._length) {
       return this.head.value;
-    }
-    return undefined;
-  }
-
-  peekBack(): T {
-    if (this._length) {
-      return this.tail.value;
     }
     return undefined;
   }
